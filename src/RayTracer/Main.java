ef1543fb2387319cee07483bd2623a06f73202a6
Astrid <astrid.vanneste1@gmail.com>
2019-01-03 19:16:29 +0100

    reflection scene

--- src/RayTracer/Main.java
+++ src/RayTracer/Main.java
@@ -14,7 +14,7 @@ public class Main
 	{
 
 		// RAYTRACER
-		RayTracer rayTracer = new RayTracer("res/JSON/refraction.json");
+		RayTracer rayTracer = new RayTracer("res/JSON/reflection.json");
 
 		// VISUALIZATION
 		JFrame frame = new JFrame("RayTracer");
